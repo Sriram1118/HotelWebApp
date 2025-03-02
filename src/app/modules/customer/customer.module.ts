45180121b5985ecdbc1047c41d1ac2a28da1d872
Sriram1118 <115363944+Sriram1118@users.noreply.github.com>
2025-03-02 15:10:25 +0530

    completed

--- src/app/modules/customer/customer.module.ts
+++ src/app/modules/customer/customer.module.ts
@@ -4,16 +4,22 @@ import { CommonModule } from '@angular/common';
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerComponent } from './customer.component';
 import { RoomsComponent } from './components/rooms/rooms.component';
+import { DemoNgZorroAntdModule } from '../../DemoNgZorroAntdModule';
+import { FormsModule } from '@angular/forms';
+import { ViewBookingsComponent } from './components/view-bookings/view-bookings.component';
 
 
 @NgModule({
   declarations: [
     CustomerComponent,
-    RoomsComponent
+    RoomsComponent,
+    ViewBookingsComponent
   ],
   imports: [
     CommonModule,
-    CustomerRoutingModule
+    CustomerRoutingModule,
+    DemoNgZorroAntdModule,
+    FormsModule
   ]
 })
 export class CustomerModule { }
